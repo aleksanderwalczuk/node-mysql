1922294272eb3b87fd5cd1c3be96ceb20f0d41a2
rednaxela700 <ohleg@windowslive.com>
2020-04-10 20:36:25 +0200

    init

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const mysql = require('mysql')
+
+const app = express()
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'alexander',
+    password: ';;',
+    database: 'acme'
+})
+
+db.connect()
+
+app.get('/users', (req, res) => {
+    const sql = 'SELECT * FROM users'
+    db.query(sql, (err, result) => {
+        if (err) {
+            throw err
+        }
+        res.send(result)
+    })
+})
+
+app.listen(5000, () => console.log('Server started'))
